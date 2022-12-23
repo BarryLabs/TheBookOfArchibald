74a536e7ab9c6c7c5d013eb9b304de964fba915a
Chandler <93629088+BarryLabs@users.noreply.github.com>
2022-12-23 12:25:42 -0500

    Create thebookofarchibald.py

---
+++ thebookofarchibald.py
@@ -0,0 +1,47 @@
+# The Book of Archibald
+
+# The Tome
+quotes = [
+    "This right here is right here.",
+    "I swear i'll piss on his grandmama's grave.",
+    "No Sir.",
+    "I've been trying to tell you!",
+]
+
+def read_quote():
+  # Choose a random quote from the list
+  quote = quotes[random.randint(0, len(quotes)-1)]
+
+  # Print the quote
+  print(quote)
+
+def add_quote(new_quote):
+  # Add the new quote to the tome
+  quotes.append(new_quote)
+
+  # Print a confirmation
+  print("Quote added:", new_quote)
+
+# Function
+def main():
+  # Print a welcome message
+  print("Welcome to The Book of Archibald!")
+
+  # Prompt the user for their choice
+  choice = input("Would you like to (R)ead a quote or (A)dd a quote? ")
+
+  # If the user wants to read a quote, call the read_quote function
+  if choice.lower() == "r":
+    read_quote()
+
+  # If the user wants to add a quote, prompt for the quote and call the add_quote function
+  elif choice.lower() == "a":
+    new_quote = input("Enter the quote: ")
+    add_quote(new_quote)
+
+  # If the user entered an invalid choice, print an error message
+  else:
+    print("Invalid choice. Please try again.")
+
+# Run
+main()
